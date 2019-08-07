6656eec4ffd760881506423ac3ee597f94db5919
Glitch (hello-webpage) <none>
2019-08-07 07:42:09 +0000

    🔨🏅 Checkpoint ./script.js:5807719/490 ./index.html:5807719/383

--- script.js
+++ script.js
@@ -3,3 +3,28 @@
 
 // prints "hi" in the browser's dev tools console
 console.log('hi');
+
+
+let word1= 'Dylan';
+let word2 = 'Isreal';
+let num1 = 2;
+let num2 = 3;
+
+const fullName = `${word1} ${word2}`
+console.log(fullName)
+
+let example = 'Hello world!';
+
+console.log(example)
+
+document.getElementById('example').innerText = example
+
+
+const personalInformation = {
+  firstName: 'Dylan',
+  lastName: 'Israel'
+}
+
+const {firstName, lastName} = personalInformation
+
+console.log(`${firstName} ${lastName}`)
